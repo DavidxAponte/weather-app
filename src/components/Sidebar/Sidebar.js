215e2b113906746aa34e260354bec17d7a252bab
David Aponte <Davidxarias321@gmail.com>
2022-07-27 14:40:39 -0300

    Finish logic of Sidebar, added request to Api.

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -6,20 +6,24 @@ import mapIcon from './map.svg'
 import magnificerIcon from './magnificer.svg'
 import axios from 'axios';
 import ApiKey from '../../api/ApiKey';
+import cityList from "../../api/city.list.json"
 
 
 export default function Sidebar(){
     const navSearch = useRef();
     const searchResultRef = useRef();
+    const cityValue = useRef();
 
     const [city, setCity] = useState("Not Found")
     const [temp, setTemp] = useState("00");
     const [weather, setWeather] = useState("Not Found");
     const [icon, setIcon]  = useState("http://openweathermap.org/img/wn/10d@2x.png")
     const [date, setDate] = useState("Not Found");
+    const [cityResult, setCityResult] = useState("Search for the city of your choice.")
 
     useEffect(() => {
      getMyLocation();
+     
     }, [])
 
     let isDisplay = false;
@@ -28,6 +32,7 @@ export default function Sidebar(){
      if(!isDisplay){
         navSearch.current.className = "search-nav activeSearch";
         isDisplay = true;
+        cityValue.current.focus();
      } else {
         navSearch.current.className = "search-nav";
         isDisplay = false;
@@ -49,8 +54,7 @@ export default function Sidebar(){
           let res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${ApiKey()}`),
           json = await res.data
           
-          console.log('this is our response data:',json)
-
+        console.log('this is our response data:',json)
         console.log("cityName: ", json.name);
          
          setCity(json.name);
@@ -59,7 +63,8 @@ export default function Sidebar(){
          setWeather(json.weather[0].description)
          setIcon(`http://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png`)
          setDate(myDate)
-         
+         setCityResult("Search for the city of your choice")
+         cityValue.current.value = "";
 
         } catch (err) {
           let message = err.statusText || "An error has ocurred";
@@ -70,24 +75,79 @@ export default function Sidebar(){
      getCityWeather();
         
     })
-   
-   
-    
    };
+
+   const getFormValue = (e) => {
+     e.preventDefault();
+     let city = cityValue.current.value;
+     console.log("this is the value: ", city); 
+     let cityInfo = locateCity(city);
+     console.log("This is the city Info:", cityInfo);
+
+     if(cityInfo !== "Not found"){
+
+      setCityResult(`${city} LOCATED!`)
+
+      getCityWeather(cityInfo);
+
+      setTimeout(displaySearch,1000);
+
+      setTimeout(displaySearch,1000);
+
+     } else{
+      setCityResult(`${city} Not Found`)
+      cityValue.current.value = "";
+     }
+   }
+   
+   const locateCity = (city) => {
+     let cityData = "Not found";
+     cityList.map((element) =>{
+       if(element.name === city){
+        cityData = element;
+       }})
+     return cityData   
+   }
+   
+   const getCityWeather = async (cityInfo) => {
+    try {
+      let res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${cityInfo.coord.lat}&lon=${cityInfo.coord.lon}&appid=${ApiKey()}`),
+      json = await res.data
+      
+    console.log('this is our response data:',json)
+    console.log("cityName: ", json.name);
+     
+     setCity(json.name);
+     let farenheit = convertKtoF(json.main.temp)
+     setTemp(`${farenheit}°F`);
+     setWeather(json.weather[0].description)
+     setIcon(`http://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png`)
+     setDate(myDate)
+     
+
+    } catch (err) {
+      let message = err.statusText || "An error has ocurred";
+      searchResultRef.current.value = message;
+      console.log(message);
+    }
+ } 
+
 
     return(
         <>
         <nav className='search-nav' ref={navSearch}>
          <button className='btnAside btn-close' onClick={displaySearch}>x</button> 
+
          <form className='search-form'>
         <div className='input-container'>
          <img src={magnificerIcon}alt="magnificerIcon" className="magnificer"></img>    
-        <input type='search' name='city' placeholder='Search Location'></input>   
+        <input type='search' name='city' placeholder='Search Location' ref={cityValue}></input>   
         </div>
-        <button className='btnAside btn-submit'>Search</button>    
+        <button className='btnAside btn-submit' onClick={getFormValue}>Search</button>    
         </form>
+
         <div className='search-results'>
-        <p ref={searchResultRef}>No results</p>
+        <p ref={searchResultRef} className="cityResult">{cityResult}</p>
         </div>  
         </nav>
 
